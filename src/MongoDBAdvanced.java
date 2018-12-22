3864ceecc9a54117cbab0be69ce875985a4c6a98
Aziiz PC <aziizpc@ymail.com>
2018-12-23 02:08:13 +0530

    MongoDB Formatted

--- src/MongoDBAdvanced.java
+++ src/MongoDBAdvanced.java
@@ -24,34 +24,34 @@ public class MongoDBAdvanced {
 		DBCollection myDBCollection = db.getCollection("EmployeeRecord"); // Case - sensitive
 
 		System.out.println("Switched to Collection !!");
-		
+
 		setUpTestData(myDBCollection);
 		System.out.println("Documents created successfully !!\n");
-		
+
 		selectFirstRecordInCollection(myDBCollection);
 		System.out.println("First document retrived successfully !!\n");
-		
+
 		selectSingleRecordAndFieldByRecordNumber(myDBCollection);
-		System.out.println("Single Record & Field By Record Number retrieved successfully !!\n");	
-		
+		System.out.println("Single Record & Field By Record Number retrieved successfully !!\n");
+
 		selectAllRecordByRecordNumber(myDBCollection);
 		System.out.println("All Record By Record Number retrieved successfully !!\n");
-		
+
 		in_Example(myDBCollection);
 		System.out.println("Printed the listed documents successfully !!\n");
-		
+
 		lessThan_GreaterThan_Example(myDBCollection);
 		System.out.println("Greater than - Less than of the record value documents retrieved successfully !!\n");
-		
+
 		negation_Example(myDBCollection);
 		System.out.println("Documents with the values not equal to the one mentioned retrieved successfully !!\n");
-		
+
 		andLogicalComparison_Example(myDBCollection);
-		System.out.println("Documents with the mentioned 'AND' retrieved successfully !!\n");	
-		
+		System.out.println("Documents with the mentioned 'AND' retrieved successfully !!\n");
+
 		regex_Example(myDBCollection);
 		System.out.println("Documents with the regex retrieved successfully !!\n");
-		
+
 	}
 
 	private static void setUpTestData(DBCollection yourDBCollection) {
@@ -60,101 +60,93 @@ public class MongoDBAdvanced {
 					.insert(new BasicDBObject().append("employeeId", i).append("employeeName", "TestEmployee_" + i));
 		}
 	}
-	
-	private static void selectFirstRecordInCollection(DBCollection yourDBCollection)
-	{
-	    DBObject dbObject = yourDBCollection.findOne();
-	    System.out.println(dbObject);
+
+	private static void selectFirstRecordInCollection(DBCollection yourDBCollection) {
+		DBObject dbObject = yourDBCollection.findOne();
+		System.out.println(dbObject);
 	}
-	
-	private static void selectSingleRecordAndFieldByRecordNumber(DBCollection yourDBCollection)
-	{
-	    BasicDBObject whereQuery = new BasicDBObject();
-	    whereQuery.put("employeeId", 3);
-	    BasicDBObject fields = new BasicDBObject();
-	    fields.put("employeeId", 1);
-	  
-	    DBCursor cursor = yourDBCollection.find(whereQuery, fields);  // Select document with employeeID 3 and print only that field. Keep 1 always in fields.
-	    while (cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void selectSingleRecordAndFieldByRecordNumber(DBCollection yourDBCollection) {
+		BasicDBObject whereQuery = new BasicDBObject();
+		whereQuery.put("employeeId", 3);
+		BasicDBObject fields = new BasicDBObject();
+		fields.put("employeeId", 1);
+
+		DBCursor cursor = yourDBCollection.find(whereQuery, fields); // Select document with employeeID 3 and print only
+																		// that field. Keep 1 always in fields.
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
-	private static void selectAllRecordByRecordNumber(DBCollection yourDBCollection)
-	{
-	    BasicDBObject whereQuery = new BasicDBObject();
-	    whereQuery.put("employeeId", 3);
-	    DBCursor cursor = yourDBCollection.find(whereQuery);
-	    while(cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void selectAllRecordByRecordNumber(DBCollection yourDBCollection) {
+		BasicDBObject whereQuery = new BasicDBObject();
+		whereQuery.put("employeeId", 3);
+		DBCursor cursor = yourDBCollection.find(whereQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
-	private static void in_Example(DBCollection yourDBCollection)
-	{
-	    BasicDBObject inQuery = new BasicDBObject();
-	 
-	    List<Integer> list = new ArrayList<Integer>();
-	    list.add(2);
-	    list.add(4);
-	    list.add(5);
-	 
-	    inQuery.put("employeeId", new BasicDBObject("$in", list));
-	 
-	    DBCursor cursor = yourDBCollection.find(inQuery);
-	    while(cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void in_Example(DBCollection yourDBCollection) {
+		BasicDBObject inQuery = new BasicDBObject();
+
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(2);
+		list.add(4);
+		list.add(5);
+
+		inQuery.put("employeeId", new BasicDBObject("$in", list));
+
+		DBCursor cursor = yourDBCollection.find(inQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
-	private static void lessThan_GreaterThan_Example(DBCollection yourDBCollection)
-	{
-	    BasicDBObject getQuery = new BasicDBObject();
-	    getQuery.put("employeeId", new BasicDBObject("$gt", 2).append("$lt", 5));
-	    DBCursor cursor = yourDBCollection.find(getQuery);
-	    while(cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void lessThan_GreaterThan_Example(DBCollection yourDBCollection) {
+		BasicDBObject getQuery = new BasicDBObject();
+		getQuery.put("employeeId", new BasicDBObject("$gt", 2).append("$lt", 5));
+		DBCursor cursor = yourDBCollection.find(getQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
-	private static void negation_Example(DBCollection yourDBCollection)
-	{
-	    BasicDBObject neQuery = new BasicDBObject();
-	    neQuery.put("employeeId", new BasicDBObject("$ne", 4));
-	    DBCursor cursor = yourDBCollection.find(neQuery);
-	    while(cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void negation_Example(DBCollection yourDBCollection) {
+		BasicDBObject neQuery = new BasicDBObject();
+		neQuery.put("employeeId", new BasicDBObject("$ne", 4));
+		DBCursor cursor = yourDBCollection.find(neQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
-	private static void andLogicalComparison_Example(DBCollection yourDBCollection)
-	{
-	    BasicDBObject andQuery = new BasicDBObject();
-	    List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
-	    obj.add(new BasicDBObject("employeeId", 2));
-	    obj.add(new BasicDBObject("employeeName", "TestEmployee_2"));
-	    andQuery.put("$and", obj);
-	  
-	    System.out.println(andQuery.toString());
-	  
-	    DBCursor cursor = yourDBCollection.find(andQuery);
-	    while (cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+
+	private static void andLogicalComparison_Example(DBCollection yourDBCollection) {
+		BasicDBObject andQuery = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("employeeId", 2));
+		obj.add(new BasicDBObject("employeeName", "TestEmployee_2"));
+		andQuery.put("$and", obj);
+
+		System.out.println(andQuery.toString());
+
+		DBCursor cursor = yourDBCollection.find(andQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
-	
+
 	private static void regex_Example(DBCollection yourDBCollection) {
-	    BasicDBObject regexQuery = new BasicDBObject();
-	    regexQuery.put("employeeName",
-	        new BasicDBObject("$regex", "TestEmployee_[3]")
-	        .append("$options", "i"));
-	  
-	    System.out.println(regexQuery.toString());
-	  
-	    DBCursor cursor = yourDBCollection.find(regexQuery);
-	    while (cursor.hasNext()) {
-	        System.out.println(cursor.next());
-	    }
+		BasicDBObject regexQuery = new BasicDBObject();
+		regexQuery.put("employeeName", new BasicDBObject("$regex", "TestEmployee_[3]").append("$options", "i"));
+
+		System.out.println(regexQuery.toString());
+
+		DBCursor cursor = yourDBCollection.find(regexQuery);
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
 	}
 
 }
