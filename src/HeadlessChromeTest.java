106f9ce7cc668260189bfe8e9aa9e0f265b560b8
Aziiz PC <aziizpc@ymail.com>
2018-12-23 13:44:21 +0530

    Headless Chrome

---
+++ src/HeadlessChromeTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HeadlessChromeTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\Azeez\\Dev\\Drivers\\chromedriver.exe");
+		
+		ChromeOptions myChromeOptions = new ChromeOptions();
+		
+		myChromeOptions.addArguments("window-size-1400,800");  // Setting window size. MANDATORY!!
+		
+		myChromeOptions.addArguments("headless");		
+
+		// Initialize browser
+		WebDriver driver = new ChromeDriver(myChromeOptions);
+
+		// Open Google
+		driver.get("https://www.google.com");
+
+		// Maximize browser
+
+		driver.manage().window().maximize();
+
+		driver.findElement(By.name("q")).sendKeys("Hello");
+		
+		System.out.println("Page Title: " + driver.getTitle());
+	}
+
+}
